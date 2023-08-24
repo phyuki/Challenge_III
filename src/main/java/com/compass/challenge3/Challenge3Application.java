5197a1d19b124a9db24aa1f7e3443637d1d44f3d
Pedro Henrique <pedrogoes06@hotmail.com>
2023-08-24 14:05:41 -0300

    Init project

---
+++ src/main/java/com/compass/challenge3/Challenge3Application.java
@@ -0,0 +1,13 @@
+package com.compass.challenge3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challenge3Application.class, args);
+	}
+
+}
